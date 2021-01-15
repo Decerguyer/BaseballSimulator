710321fadb8558a5927d8078fc48213c7632d36b
jmayor <jmayor@umich.edu>
2021-01-15 12:58:34 -0500

    Added ImageData class and a few minor changes to the camera class for getting intrinsics and returning images. Included beginning of the tracker class set up but it's not done.

--- baseball-simulator/BaseballSimulator.h
+++ baseball-simulator/BaseballSimulator.h
@@ -11,6 +11,7 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <librealsense2/rsutil.h> // Include RealSense Cross Platform API
 #include <opencv2/opencv.hpp>
+#include <deque>
 
 cv::Mat rsDepthToCVMat(rs2::depth_frame depthFrame){
     auto pf = depthFrame.get_profile().as<rs2::video_stream_profile>();
@@ -24,6 +25,63 @@ cv::Mat rsDepthToCVMat(rs2::depth_frame depthFrame){
     return depth_metric_fp;
 }
 
+class ImageData{
+public:
+    ImageData(rs2::frameset fSet){
+        depthMat = rsDepthToCVMat(fSet.get_depth_frame());
+        irMat = rsIRToCVMat(fSet.get_infrared_frame(1));
+        timeStamp = fSet.get_depth_frame().get_frame_metadata(RS2_FRAME_METADATA_SENSOR_TIMESTAMP);
+        frameNumber = fSet.get_depth_frame().get_frame_number();
+    }
+    
+    cv::Mat getDepthMat(){
+        return depthMat;
+    }
+    
+    cv::Mat getIRMat(){
+        return irMat;
+    }
+    
+    float getDepthAt(int pixelX, int pixelY){
+        return depthMat.at<float>(pixelY, pixelX);
+    }
+    
+    long long getTimeStamp(){
+        return timeStamp;
+    }
+    
+    unsigned long long getFrameNumber(){
+        return frameNumber;
+    }
+    
+private:
+    cv::Mat depthMat;
+    cv::Mat irMat;
+    
+    long long timeStamp;
+    unsigned long long frameNumber;
+
+    
+    cv::Mat rsDepthToCVMat(rs2::depth_frame depthFrame){
+        rs2::video_stream_profile prof = depthFrame.get_profile().as<rs2::video_stream_profile>();
+        cv::Mat depth_metric_fp;
+        cv::Mat depth_raw = cv::Mat(prof.height(), prof.width(), CV_16SC1, const_cast<void*>(depthFrame.get_data()));
+        depth_raw.convertTo(depth_metric_fp, CV_32FC1);
+        depth_metric_fp *= depthFrame.get_units();
+        
+        
+        return depth_metric_fp;
+    }
+    
+    cv::Mat rsIRToCVMat(rs2::video_frame irFrame){
+        rs2::video_stream_profile prof = irFrame.get_profile().as<rs2::video_stream_profile>();
+        cv::Mat ir_mat = cv::Mat(prof.height(), prof.width(), CV_8UC1, (void*)irFrame.get_data());
+        return ir_mat;
+    }
+    
+    
+};
+
 
 class Camera{
 public:
@@ -36,6 +94,8 @@ public:
         rs2::device dev = devList.front();
         
         depthSensor = dev.first<rs2::depth_sensor>();
+        
+        intrin = depthSensor.get_stream_profiles().front().as<rs2::video_stream_profile>().get_intrinsics();
     }
     
     void setExposure(int exposureTime){
@@ -66,10 +126,10 @@ public:
     }
     
     /*Takes specified number of frames and
-     returns a vector of the collected frames
+     returns a deque of the collected frames
      */
-    std::vector<rs2::frameset> recordRSFrames(int numFrames,int numThrow = 20){
-        std::vector<rs2::frameset> frames;
+    std::deque<rs2::frameset> recordRSFrames(int numFrames,int numThrow = 20){
+        std::deque<rs2::frameset> frames;
         
         pipe.start(cfg);
         throwFrames(numThrow);
@@ -79,9 +139,20 @@ public:
             fSet.keep();
             frames.push_back(fSet);
         }
-        intrin = pipe.get_active_profile().get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>().get_intrinsics();
         pipe.stop();
         return frames;
+    }
+    
+    std::deque<ImageData> recordImageData(int numFrames,int numThrow = 20){
+        std::deque<rs2::frameset> recordedFrames = recordRSFrames(numFrames, numThrow);
+        
+        std::deque<ImageData> images;
+        while(!recordedFrames.empty()){
+            images.emplace_back(recordedFrames.front());
+            recordedFrames.pop_front();
+        }
+        
+        return images;
     }
 
     //returns the most recent frame
@@ -104,7 +175,7 @@ public:
             getFrame();
         }
     }
-    struct rs2_intrinsics getIntrin(){
+    struct rs2_intrinsics getIntrinsics(){
     	return intrin;
     }
     
@@ -175,21 +246,64 @@ private:
     }
 };
 
-//class Tracker{
-//public:
-//    Tracker(int width, int height){
-//        ROI = cv::Rect(0, 0, width - 1, height - 1);
-//        imageSize = cv::Rect(0, 0, width - 1, height - 1);
-//
-//    }
-//private:
-//    float bndBoxSize = 180;
-//    
-//    cv::Rect ROI;
-//    cv::Rect imageSize;
-//
-//    
-//};
+
+class Tracker{
+public:
+    Tracker(int width, int height){
+        ROI = cv::Rect(0, 0, width - 1, height - 1);
+        imageSize = cv::Rect(0, 0, width - 1, height - 1);
+        inBoundsROI = ROI & imageSize;
+    }
+    
+    //returns a Mat with a circle drawn on it
+    cv::Mat drawCircle(cv::Mat image, cv::Vec3f coord){
+        cv::Mat retMat = image.clone();
+        cv::Point pnt(coord[0], coord[1]);
+        
+        //draw the outer circle //green
+        circle(retMat, pnt, coord[2], cv::Scalar(0,255,0), 2, 8, 0 );
+        //draw the center of the circle //red
+        circle(retMat, pnt, 2 , cv::Scalar(0,0,255), 2, 8, 0 );
+        
+        return retMat;
+    }
+private:
+    float bndBoxSize = 180;
+    
+    cv::Rect ROI;
+    cv::Rect imageSize;
+    cv::Rect inBoundsROI;
+    
+    int ballPixX;
+    int ballPixY;
+    
+    //returns pixel x [0], pixel y [1], and the radius[0]
+    cv::Vec3f findBall(cv::Mat image, double distance){
+        std::vector<cv::Vec3f> coords;
+
+        int radius = ballRadius(distance);
+        
+        //Should eventually refine parameters again
+        cv::HoughCircles(image, coords, cv::HOUGH_GRADIENT, 1.6, 4000, 50, 5, (radius - radius*0.15), radius + radius*0.15);
+        
+        return coords[0];
+    }
+    
+    // Returns the radius of the ball
+    // Needs expansion
+    int ballRadius(float ballDepth){
+        int radius = (27*0.646)/ballDepth;
+        
+        if (radius > 25){
+            radius = 25;
+        }
+    }
+    
+    void adjustROI(){
+        
+    }
+    
+};
 
 
 #endif /* BaseballSimulator_h */
