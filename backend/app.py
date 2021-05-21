a3b6b2a589a1ac603e4cefd74c063f0c7841997e
Owen Loveluck <owen.loveluck@1up.health>
2021-05-21 16:01:00 -0400

    fix getting pitches with new format

--- backend/app.py
+++ backend/app.py
@@ -45,7 +45,8 @@ class Pitch:
             'error': { 'L': dynamo_error },
             'serial_number': { 'N': str(self.serial_number) },
             'pitcher_id': { 'S': str(self.pitcher_id) },
-            'pitch_id': { 'S': str(self.pitch_id) }            
+            'pitch_id': { 'S': str(self.pitch_id) },
+            'time': { 'S': str(self.time) }            
         }
         
 
@@ -81,45 +82,59 @@ def hello():
 
 @app.route("/pitch/<string:pitch_id>")
 def get_user(pitch_id):
-    resp = client.get_item(
-        TableName=PICHES_TABLE,
-        Key={
-            'pitch_id': {'S': pitch_id}
-        }
-    )
-    item = resp.get('Item')
-    if not item:
-        return jsonify({'error': 'Pitch does not exist'}), 404
+    try:
+        resp = client.get_item(
+            TableName=PICHES_TABLE,
+            Key={
+                'pitch_id': {'S': pitch_id}
+            }
+        )
+        pitch = resp.get('Item')
+        if not pitch:
+            return jsonify({'error': 'Pitch does not exist'}), 404
 
-    # {
-    #     'pitch_id': item.get('pitch_id').get('S'),
-    #     'positions': item.get('positions').get('NS'),
-    #     'spin': item.get('spin').get('N'),
-    #     'timestamp': item.get('time').get('S'),
-    #     'pitcher': item.get('pitcher').get('S')
-    # }
+        return jsonify(convert_pitch_to_response(pitch)), 200
+    except Exception as e:
+        return jsonify({ 'error': str(e) }), 400
 
-    return jsonify(item), 200
+def convert_pitch_to_response(pitch):
+    positions = []
+    timestamps = []
+    errors = []
+    spins = []
+    for position in pitch.get('positions').get('L'):
+        positions.append([float(position.get('M')['x'].get('N')), float(position.get('M')['y'].get('N')), float(position.get('M')['z'].get('N'))])
+    for error in pitch.get('error').get('L'):
+        errors.append([float(error.get('M')['x'].get('N')), float(error.get('M')['y'].get('N')), float(error.get('M')['z'].get('N'))])
+    for timestamp in pitch.get('timestamps').get('L'):
+        timestamps.append(float(timestamp.get('N')))
+    spin = [float(pitch.get('spin').get('M')['x'].get('N')), float(pitch.get('spin').get('M')['y'].get('N')), float(pitch.get('spin').get('M')['z'].get('N'))] 
+
+    return {
+        'pitch_id': pitch.get('pitch_id').get('S'),
+        'positions': positions,
+        'timestamps': timestamps,
+        'error': errors,
+        'spin': spin,
+        'serial_number': int(pitch.get('serial_number').get('N')),
+        'time': pitch.get('time').get('S'),
+        'pitcher_id': pitch.get('pitcher_id').get('S')
+    }
 
 
 @app.route("/pitch", methods=["GET"])
 def get_pitch_history():
-    resp = client.scan(
-        TableName=PICHES_TABLE
-    )
-    result = []
-    print('Response: ', resp)
-    # for pitch in resp.get('Items'):
-    #     # result.append({
-    #     #     'pitch_id': pitch.get('pitch_id').get('S'),
-    #     #     'positions': pitch.get('positions').get('NS'),
-    #     #     'spin': pitch.get('spin').get('N'),
-    #     #     'timestamp': pitch.get('time').get('S'),
-    #     #     'pitcher': pitch.get('pitcher').get('S')
-    #     # })
-
-    return jsonify(resp.get('Items')), 200
-
+    try:
+        resp = client.scan(
+            TableName=PICHES_TABLE
+        )
+        result = []
+        print('Response: ', resp)
+        for pitch in resp.get('Items'):
+            result.append(convert_pitch_to_response(pitch))
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({ 'error': str(e) }), 400
 
 @app.route("/pitch", methods=["POST"])
 def record_pitch():
