7357104e9765502f30c85e008174b31b9a991280
Decerguyer <yoni.arieh@gmail.com>
2021-05-20 20:00:12 -0400

    Readd fucked files

--- KalmanGit/JSON/Class Based UKF/V2/UKFB_Routine.py
+++ KalmanGit/JSON/Class Based UKF/V2/UKFB_Routine.py
@@ -1,10 +1,14 @@
-from JSONControl import JSONControl
+from HTTPHandler import HTTPHandler
 from UKFB import UKFB
 from BaseballPhysicalModel import BaseballPhysicalModel
+from JSONControl import JSONControl
 
-json_manager = JSONControl()
-json_data = json_manager.get_input_json("SimulatedNoisyPitch.json")
+http_handler = HTTPHandler("owen gay")
+json_data = http_handler.get_most_recent_pitch()
 
+print(json_data)
+print(json_data["positions"])
+'''
 UKF = UKFB(json_data)
 UKF.control_loop()
 kalman_output = UKF.output_dict()
@@ -12,4 +16,7 @@ kalman_output = UKF.output_dict()
 PM = BaseballPhysicalModel(kalman_output)
 PM.physical_model_control()
 PM.print_output()
+
+json_manager = JSONControl()
 json_manager.write_output_json("OutputFile.json", PM.positionVector)
+'''
