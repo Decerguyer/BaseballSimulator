5764900caa18daaaa1cd6023312f67c78b343158
Decerguyer <yoni.arieh@gmail.com>
2021-07-08 09:56:00 -0400

    Trying to fix ImageReconditioning Compilation error
    possible that extra includes are causing problems

--- baseball-simulator/ImageReconditioning.cpp
+++ baseball-simulator/ImageReconditioning.cpp
@@ -1,4 +1,3 @@
-#include "BaseballSimulator.h"
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
--- baseball-simulator/ImageReconditioning.h
+++ baseball-simulator/ImageReconditioning.h
@@ -8,7 +8,6 @@
 #ifndef ImageReconditioning_h
 #define ImageReconditioning_h
 
-#include "BaseballSimulator.h"
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
