ab063db57f3064e5b55b781cccfa9e1652fb36ae
Decerguyer <yoni.arieh@gmail.com>
2021-09-14 12:06:48 -0400

    frontend

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Table from "./Table";
+import Container from "./3dpitch";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Header} from "semantic-ui-react";
+
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            pitches: [{
+                "pitcher_id":"",
+                "error": [],
+                "pitch_id":"",
+                "positions": [],
+                "serial_number": "",
+                "spin":[],
+                "time":"",
+                "timestamps":[],
+                }],
+        };
+    }
+
+    render(){
+        const {isLoaded, pitches} = this.state;
+        return (
+            isLoaded ?
+                <div>
+                    <Header>Pitches Table</Header>
+                    {pitches.length > 0 && <><div><Table pitches={pitches} /></div>
+                        <Container pitches={pitches}/></>}
+                </div>:
+                <div/>
+        )
+    }
+
+
+    componentDidMount() {
+        fetch('/pitch')
+            .then(res => res.json())
+            .then(data=>{
+                this.setState({pitches: data, isLoaded: true})
+            })
+            .catch(console.log);
+    };
+
+
+}
+
+
+
+export default Home;
